62d884e9b23f6d48f3af9c0896481b720e96d595
coffeetron832 <jpontonh@gmail.com>
2025-07-25 14:22:16 -0500

    Update videoRoutes.js

--- server/routes/videoRoutes.js
+++ server/routes/videoRoutes.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../middleware/reviewQueue');
+const auth = require('../middleware/auth');
+
 const {
   uploadVideo,
   getApprovedVideos,
@@ -9,9 +11,12 @@ const {
   updateVideoStatus
 } = require('../controllers/videoController');
 
+// Solo usuarios autenticados pueden subir videos
+router.post('/upload', auth, upload.single('video'), uploadVideo);
 router.post('/upload', upload.single('video'), uploadVideo);
 router.get('/explore', getApprovedVideos);
 router.get('/pending', getPendingVideos); // Ruta para panel admin
 router.put('/review/:id', updateVideoStatus); // Cambiar estado del video
+
 
 module.exports = router;
