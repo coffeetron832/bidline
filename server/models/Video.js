cc2cb347d9a4788e9903f27ee07178704cdf7133
coffeetron832 <jpontonh@gmail.com>
2025-07-25 14:53:19 -0500

    Update Video.js

--- server/models/Video.js
+++ server/models/Video.js
@@ -1,12 +1,20 @@
-// server/models/Video.js
 const mongoose = require('mongoose');
 
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: String,
   filename: { type: String, required: true },
-  status: { type: String, enum: ['pendiente', 'aprobado', 'rechazado'], default: 'pendiente' },
-  uploadedAt: { type: Date, default: Date.now }
+  status: {
+    type: String,
+    enum: ['pendiente', 'aprobado', 'rechazado'],
+    default: 'pendiente'
+  },
+  uploadedAt: { type: Date, default: Date.now },
+  uploader: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
 });
 
 module.exports = mongoose.model('Video', videoSchema);
