3e4389c85f396e8a5097c6a452af3470350642ff
coffeetron832 <jpontonh@gmail.com>
2025-07-25 11:52:57 -0500

    Update videoRoutes.js

--- server/routes/videoRoutes.js
+++ server/routes/videoRoutes.js
@@ -2,9 +2,16 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../middleware/reviewQueue');
-const { uploadVideo, getApprovedVideos } = require('../controllers/videoController');
+const {
+  uploadVideo,
+  getApprovedVideos,
+  getPendingVideos,
+  updateVideoStatus
+} = require('../controllers/videoController');
 
 router.post('/upload', upload.single('video'), uploadVideo);
 router.get('/explore', getApprovedVideos);
+router.get('/pending', getPendingVideos); // Ruta para panel admin
+router.put('/review/:id', updateVideoStatus); // Cambiar estado del video
 
 module.exports = router;
